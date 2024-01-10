defebecc59e1f6e9f4cdc6249b609009b1564a0c
Maria Lachinina <masha.lachinina@mail.ru>
2024-01-10 19:53:42 +0300

    исправлены замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
+const placesList = document.querySelector('.places__list');
+
 function createCard(card, deleteCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = card.link;
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = card.link;
+  cardImage.alt = card.alt;
   cardElement.querySelector('.card__title').textContent = card.name;
   cardElement.querySelector('.card__delete-button').addEventListener('click', evt => deleteCard(evt));
-  const placesList = document.querySelector('.places__list');
+  return cardElement;
+}
+
+function addCard(cardElement) {
   placesList.append(cardElement);
 }
 
@@ -12,4 +19,4 @@ function deleteCard(event) {
   event.target.parentElement.remove();
 }
 
-initialCards.forEach(item => createCard(item, deleteCard));
+initialCards.forEach(item => addCard(createCard(item, deleteCard)));
